36dc7f29b81c363bf56c554b6c8a2a01fa3b5ab8
Dave Richmond <daverichmond@gmail.com>
2014-08-27 18:46:34 +0200

    Update features to account for input from multiple images (assumes all images have the same size).

--- include/vigra/random_forest/features.hxx
+++ include/vigra/random_forest/features.hxx
@@ -37,16 +37,18 @@ template<class T, class C = StridedArrayTag>
 class NormalFeatures : public FeatureBase<T,C>
 {
 public:
+    typedef FeatureBase<T,C>    FB;
+
     NormalFeatures(MultiArrayView<2, T, C> const & original,
                    MultiArrayShape<2>::type  im_shape)
-        : FeatureBase<T,C>(original, im_shape)
+        : FB(original, im_shape)
     {}
 
     ~NormalFeatures() { }
 
     T operator() (int i, int j) const
     {
-        return FeatureBase<T,C>::original_(i, j);
+        return FB::original_(i, j);
     }
 
 };
@@ -55,13 +57,15 @@ template<class T, class C = StridedArrayTag>
 class OffsetFeatures : public FeatureBase<T,C>
 {
 public:
+    typedef FeatureBase<T,C>    FB;
+
     int offset_x_, offset_y_;
 
     OffsetFeatures(MultiArrayView<2, T, C> const & original,
                    MultiArrayShape<2>::type im_shape,
                    int offset_x,
                    int offset_y)
-        : FeatureBase<T,C>(original, im_shape)
+        : FB(original, im_shape)
     {
         offset_x_ = offset_x;
         offset_y_ = offset_y;
@@ -71,22 +75,30 @@ public:
 
     T operator() (int i, int j) const
     {
+        // there may be multiple images passed in simultaneously, appended into a big feature array.  calc corresponding offset and apply.
+        int L = FB::im_shape_[0]*FB::im_shape_[1];
+        int im_offset = static_cast<int>(floor(i/L))*L;
+        i -= im_offset;
+
         // convert index i (sample #) to x,y position in image, and then shift
-        int x = i % FeatureBase<T,C>::im_shape_[0];
-        int y = static_cast<int>(floor(i/FeatureBase<T,C>::im_shape_[0]));
+        int x = i % FB::im_shape_[0];
+        int y = static_cast<int>(floor(i/FB::im_shape_[0]));
         int xp = x + offset_x_;
         int yp = y + offset_y_;
 
         // deal with out of bounds indices.  just move them back to the border of the image.
         // IN FUTURE, DO SOMETHING SMARTER HERE.  E.G., REFLECT IMAGE OVER THE BORDER?
-        if (xp >= FeatureBase<T,C>::im_shape_[0]) xp = FeatureBase<T,C>::im_shape_[0]-1;
+        if (xp >= FB::im_shape_[0]) xp = FB::im_shape_[0]-1;
         else if (xp < 0) xp = 0;
-
-        if (yp >= FeatureBase<T,C>::im_shape_[1]) yp = FeatureBase<T,C>::im_shape_[1]-1;
+        if (yp >= FB::im_shape_[1]) yp = FB::im_shape_[1]-1;
         else if (yp < 0) yp = 0;
 
-        // convert back to index into feature array
-        return FeatureBase<T,C>::original_(yp * FeatureBase<T,C>::im_shape_[0] + xp, j);
+        // convert back to index into feature array, and correct for initial offset.  note: i is not corrected, b/c not used again.
+        int ip = yp * FB::im_shape_[0] + xp;
+        ip += im_offset;
+
+        // return
+        return FB::original_(ip, j);
     }
 
 };
@@ -95,13 +107,15 @@ template<class T, class C = StridedArrayTag>
 class DiffFeatures : public FeatureBase<T,C>
 {
 public:
+    typedef FeatureBase<T,C>    FB;
+
     int offset_x_, offset_y_;
 
     DiffFeatures(MultiArrayView<2, T, C> const & original,
                    MultiArrayShape<2>::type im_shape,
                    int offset_x,
                    int offset_y)
-        : FeatureBase<T,C>(original, im_shape)
+        : FB(original, im_shape)
     {
         offset_x_ = offset_x;
         offset_y_ = offset_y;
@@ -111,22 +125,31 @@ public:
 
     T operator() (int i, int j) const
     {
+        // there may be multiple images passed in simultaneously, appended into a big feature array.  calc corresponding offset and apply.
+        int L = FB::im_shape_[0]*FB::im_shape_[1];
+        int im_offset = static_cast<int>(floor(i/L))*L;
+        i -= im_offset;
+
         // convert index i (sample #) to x,y position in image, and then shift
-        int x = i % FeatureBase<T,C>::im_shape_[0];
-        int y = static_cast<int>(floor(i/FeatureBase<T,C>::im_shape_[0]));
+        int x = i % FB::im_shape_[0];
+        int y = static_cast<int>(floor(i/FB::im_shape_[0]));
         int xp = x + offset_x_;
         int yp = y + offset_y_;
 
         // deal with out of bounds indices.  just move them back to the border of the image.
         // IN FUTURE, DO SOMETHING SMARTER HERE.  E.G., REFLECT IMAGE OVER THE BORDER?
-        if (xp >= FeatureBase<T,C>::im_shape_[0]) xp = FeatureBase<T,C>::im_shape_[0]-1;
+        if (xp >= FB::im_shape_[0]) xp = FB::im_shape_[0]-1;
         else if (xp < 0) xp = 0;
-
-        if (yp >= FeatureBase<T,C>::im_shape_[1]) yp = FeatureBase<T,C>::im_shape_[1]-1;
+        if (yp >= FB::im_shape_[1]) yp = FB::im_shape_[1]-1;
         else if (yp < 0) yp = 0;
 
-        // convert back to index into feature array
-        return FeatureBase<T,C>::original_(yp * FeatureBase<T,C>::im_shape_[0] + xp, j) - FeatureBase<T,C>::original_(i, j);
+        // convert back to index into feature array, and correct for initial offset
+        int ip = yp * FB::im_shape_[0] + xp;
+        ip += im_offset;
+        i += im_offset;
+
+        // return
+        return FB::original_(ip, j) - FB::original_(i, j);
     }
 };
 
